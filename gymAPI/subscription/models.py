97bbdbca4e9a9188c6a107924d7f269cdace6f78
Mahmoud Shehab <mahmood.x.mostafa@gmail.com>
2022-08-06 13:22:34 +0200

    Plan & Subscription Models

---
+++ gymAPI/subscription/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+# Create your models here.
+class Plan(TimeStampedModel, models.Model):
+    name = models.fields.CharField(max_length=40)
+    duration_months = models.fields.IntegerField()
+    price = models.fields.FloatField()
+    pass
+
+
+class Subscription(TimeStampedModel, models.Model):
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.fields.DateTimeField(null=True)
+    end_date = models.fields.DateTimeField(null=True)
+    plan_id = models.ForeignKey(Plan, on_delete=models.CASCADE)
