43a5a1b8baae97d38af0082f5df25f13ca2bf944
“Mohamed <mahmadoshokr@gmail.com>
2022-08-06 19:06:15 +0200

    events_model

--- gymAPI/events/models.py
+++ gymAPI/events/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import FileExtensionValidator
+from django.contrib.auth.models import AbstractUser
+from djmoney.models.fields import MoneyField
+
 
 # Create your models here.
+class User(AbstractUser): #to Delete#################<<<<<<<<<<<<<<<<<<<<----------------------
+    pass
+ 
+ 
+class Event(models.Model):
+    event_name = models.CharField(max_length=50, null=False)
+    description = models.TextField()
+    
+    # photo = models.ImageField(upload_to='events/', null=True, blank=True , default='static/icons/new_event.jpg' ,validators=[FileExtensionValidator(['svg', 'jpg', 'jpeg', 'png', 'gif' ])])
+   
+    photo = models.ImageField(null=True, blank=True ,validators=[FileExtensionValidator(['svg', 'jpg', 'jpeg', 'png', 'gif' ])])
+
+    num_of_participants = models.IntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now, auto_now_add=True)
+    starts_at = models.DateTimeField(blank=True, null=True)
+    ends_at = models.DateTimeField(blank=True, null=True)
+    price = MoneyField(max_digits=6, decimal_places=2, default_currency='EGP', null=True)
+    
+    def __str__(self):
+        return self.event
+    
+    class Meta:
+        ordering = ['created_at']
+    
+
+class EventParticipants(models.Model):
+    participant = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
