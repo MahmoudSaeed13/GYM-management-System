20581effdc088175770ae35efc83a389e3908acc
Mahmoud Shehab <mahmood.x.mostafa@gmail.com>
2022-08-08 15:24:28 +0200

    update

--- gymAPI/subscription/api/serializers.py
+++ gymAPI/subscription/api/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from subscription.models import Subscription, Plan
 
 
-class SubscriptionSerializer(serializers.HyperlinkedModelSerializer):
+class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
         fields = "__all__"
 
 
-class PlanSerializer(serializers.HyperlinkedModelSerializer):
+class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
         fields = "__all__"
