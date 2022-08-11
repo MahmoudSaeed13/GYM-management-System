948ffb33ce889e3757ba566ca2075cfd47b5ca83
Mahmoud Shehab <mahmood.x.mostafa@gmail.com>
2022-08-11 16:12:58 +0200

    add permission & refactor subscription model

--- gymAPI/subscription/api/views.py
+++ gymAPI/subscription/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from subscription.models import Subscription, Plan
 from subscription.api.serializers import SubscriptionSerializer, PlanSerializer
 
@@ -6,8 +7,10 @@ from subscription.api.serializers import SubscriptionSerializer, PlanSerializer
 class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class PlanViewSet(viewsets.ModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
+    permission_classes = [IsAdminUser]
