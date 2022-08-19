162a353c8b18a388d173f7fd8ebbadaf8d9d501f
“Mohamed <mahmadoshokr@gmail.com>
2022-08-19 13:42:06 +0200

    classes modified

--- gymAPI/classes/api/serializers.py
+++ gymAPI/classes/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from classes.models import Class
+from classes.models import Attendant
 
 class ClassSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,3 +8,22 @@ class ClassSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "description", "price","created"]
 
 
+class AttendantSerializer(serializers.ModelSerializer):
+    clas = serializers.CharField()
+    attendant = serializers.CharField(read_only=True)
+    class Meta:
+        model = Attendant
+        fields = ["attendant","clas", "subscribe_status"]
+
+    def __init__(self, *args, **kwargs):
+        data = kwargs['data']
+        self.attendant = data['attendant']
+        super().__init__(*args, **kwargs)
+
+    def create(self, validated_data):
+        return Attendant.objects.create(
+            attendant = self.attendant,
+            clas = Class.objects.get(name=validated_data.get('clas')),
+            subscribe_status = validated_data.get("subscribe_status")
+        )
+        
--- gymAPI/classes/api/urls.py
+++ gymAPI/classes/api/urls.py
@@ -1,9 +1,10 @@
 from classes.api.views import *
-from django.urls import path
 from rest_framework.routers import DefaultRouter
+from . import views
 
 router = DefaultRouter()
 
 router.register('', ClassViewSet, basename="classes")
+router.register("", views.AttendantViewSet, basename="attendant")
 
 urlpatterns = router.urls
