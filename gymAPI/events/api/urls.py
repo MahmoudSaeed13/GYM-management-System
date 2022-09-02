288254aea90dae161d1c5f47acf45007a2c9f739
Mahmoud Shehab <mahmood.x.mostafa@gmail.com>
2022-09-03 00:43:15 +0200

    update urls

--- gymAPI/events/api/urls.py
+++ gymAPI/events/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 
-router.register("", views.ParticipantsViewSet, basename="participants")
+router.register("participants", views.ParticipantsViewSet, basename="participants")
 
 urlpatterns = [
     path("", views.EventListView.as_view(), name="events-list"),
     path("create/", views.EventCreateView.as_view(), name="events-create"),
-    path('Detail/<int:pk>/', views.EventDetailView.as_view(), name="event-detail"),
-    path('modify/<int:pk>/', views.EventUpdateDestroyView.as_view(), name="event-update-delete"),
+    path("Detail/<int:pk>/", views.EventDetailView.as_view(), name="event-detail"),
+    path("modify/<int:pk>/", views.EventUpdateDestroyView.as_view(), name="event-update-delete"),
 ]
 
 urlpatterns += router.urls
